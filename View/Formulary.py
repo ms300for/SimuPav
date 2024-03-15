4a90d7ab5ffef368b772c86a41621cc7caf0c3ee
MateusAval <mateus.serafim@avalengenharia.eng.br>
2024-03-15 14:06:09 -0300

    WIP formulary

--- View/Formulary.py
+++ View/Formulary.py
@@ -1,24 +1,30 @@
 from tkinter import *
 
 def CreateProjectFormulary(app):
-    email = StringVar()
+    project_name = StringVar()
     
     app.WindowsForm = Toplevel()
     app.WindowsForm.title("Criar Projeto")
-    
-    app.base_frame = Frame(app.WindowsForm)
-    app.base_frame.pack(padx=40, pady=10, fill='x', expand=True)
-    
-    app.label = Label(app.base_frame, text='Insira as informações do projeto')
-    app.label.grid()
-    
-    app.ok_button = Button(app.base_frame, text="criar")
-    app.ok_button.grid()
-    app.close_button = Button(app.base_frame, text='Fechar', command=lambda: app.WindowsForm.destroy())
-    app.close_button.grid()
-    
-    app.WindowsForm.geometry('300x200')
+    app.WindowsForm.geometry('300x150')
     app.WindowsForm.resizable(False, False)
     app.WindowsForm.transient(app)
     app.WindowsForm.focus_force()
     app.WindowsForm.grab_set()
+    
+    app.base_frame = Frame(app.WindowsForm)
+    app.base_frame.pack(padx=5, pady=5, fill='x', expand=True)
+
+    app.base_frame.columnconfigure(0, weight=1)
+    app.base_frame.columnconfigure(1, weight=5)
+    
+    app.label_project_name = Label(app.base_frame, text='Nome do projeto: ')
+    app.label_project_name.grid(column=0, row=0, sticky=W, padx=5, pady=5)
+
+    app.project_name_entry = Entry(app.base_frame, textvariable=project_name)
+    app.project_name_entry.grid(column=1, row=0, sticky=W, padx=5, pady=5)
+    
+    app.ok_button = Button(app.base_frame, text="criar")
+    app.ok_button.grid(column=1, row=1, sticky=W, padx=5, pady=5)
+
+    app.close_button = Button(app.base_frame, text='Fechar', command=lambda: app.WindowsForm.destroy())
+    app.close_button.grid(column=1, row=1, padx=5, pady=5)
