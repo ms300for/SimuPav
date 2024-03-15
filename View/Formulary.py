f879082bed6b12c55639d79510b01489482ce849
ms300for <mateuserafim3@gmail.com>
2024-03-14 22:39:02 -0300

    Change formulary

--- View/Formulary.py
+++ View/Formulary.py
@@ -1,13 +1,24 @@
 from tkinter import *
 
 def CreateProjectFormulary(app):
+    email = StringVar()
+    
     app.WindowsForm = Toplevel()
     app.WindowsForm.title("Criar Projeto")
-    app.l=Label(app.WindowsForm, text='Insira as informações do projeto')
-    app.l.grid()
-    b=Button(app.WindowsForm, text='Fechar', command=lambda: app.WindowsForm.destroy())
-    b.grid()
+    
+    app.base_frame = Frame(app.WindowsForm)
+    app.base_frame.pack(padx=40, pady=10, fill='x', expand=True)
+    
+    app.label = Label(app.base_frame, text='Insira as informações do projeto')
+    app.label.grid()
+    
+    app.ok_button = Button(app.base_frame, text="criar")
+    app.ok_button.grid()
+    app.close_button = Button(app.base_frame, text='Fechar', command=lambda: app.WindowsForm.destroy())
+    app.close_button.grid()
+    
     app.WindowsForm.geometry('300x200')
-    app.WindowsForm.transient(app)#
-    app.WindowsForm.focus_force()#
-    app.WindowsForm.grab_set()#
+    app.WindowsForm.resizable(False, False)
+    app.WindowsForm.transient(app)
+    app.WindowsForm.focus_force()
+    app.WindowsForm.grab_set()
